2029a932fde75fda914fd7e8f3fdd343c4809d5f
Calixte Suaudeau <obissan.suaudeau@gmail.com>
2024-02-23 13:27:58 +0100

    [Azure] +Trigger function for azure communication

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -1,3 +1,8 @@
+
+https://get.geojs.io/v1/ip/geo.json
+
+const { app } = require('@azure/functions');
+
 const latitude = null;
 const longitude = null;
 
@@ -16,3 +21,15 @@ function success(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
 }
+
+app.http('httpTrigger1', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    route: 'vehicules/{id:int?}',
+    handler: async (request, context) => {
+        const date = Date();    
+        const id = request.params.id;
+
+        return { body: `Latitude: ${latitude}, Longitude: ${longitude}, Date: ${date}, ID: ${id}` };
+    }
+});
